ba47f234e8bb0f5ba0a6b960040086e1a4c6230d
Justin <justin@justin.mac>
2018-09-25 12:43:46 -0400

    add new protos from DoctorMcKay/node-steam-resources

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ Steam.Internal = loadProtoFiles([
   'steamclient/steammessages_base.proto',
   'steamclient/encrypted_app_ticket.proto',
   'steamclient/steammessages_clientserver.proto',
-  'steamclient/steammessages_clientserver_2.proto'
+  'steamclient/steammessages_clientserver_2.proto',
+  'steamclient/steammessages_clientserver_friends.proto',
+  'steamclient/steammessages_clientserver_login.proto'
 ]);
 
 Steam.GC = {
